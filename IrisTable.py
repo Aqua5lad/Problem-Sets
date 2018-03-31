d171fc52366d9b38bbacc52111d132a7d8ef6ef1
Colm Doherty <G00364795@gmit.ie>
2018-03-31 17:47:54 +0100

    Update and rename Exercise 5 - Iris data set tabulation to IrisTable.py

--- IrisTable.py
+++ IrisTable.py
@@ -1,3 +1,5 @@
+# Exercise 5
+
 # Colm Doherty on 2018-03-05
 # Exercise 5 - a python script that reads the Iris dataset and prints 
 # the four numerical values on each row in an nice format, so, petal length,
