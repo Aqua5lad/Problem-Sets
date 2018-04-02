dcd77a9ebcc586ed47bccea0b55d499fe95a310b
Colm Doherty <G00364795@gmit.ie>
2018-04-02 12:11:11 +0100

    commented code

--- IrisTable.py
+++ IrisTable.py
@@ -7,6 +7,17 @@
 # a space between the columns. 
 
 with open("data/iris.csv") as f:
-    for line in f:        
-        print('{:4} {:4} {:4} {:4}'.format(line.split(',')[0], line.split(',')[1], line.split(',')[2], line.split(',')[3]))
+            # open(filename) returns the file to be used, in this case the Iris dataset.
+            # For best practice, its prefaced by 'with' to ensure the file is closed afterwards
 
+    for line in f:        
+            # for each line in the dataset:
+
+        print('{:4} {:4} {:4} {:4}'.format(line.split(',')[0], line.split(',')[1], line.split(',')[2], line.split(',')[3]))
+        
+            # split it into its four constituent elements (petal length, petal width, sepal length, sepa width) and
+            # add the data for each, using the {:n} syntax to give each printed element a width of n, where n is {:n}
+                
+
+                
+# When you're chewing on life's gristle, Don't grumble, give a whistle   
