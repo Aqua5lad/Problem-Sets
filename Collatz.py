3eff27e16fab6d86272e89444ed3b9f2decc0912
Colm Doherty <G00364795@gmit.ie>
2018-04-08 14:19:33 +0100

    comment edit

--- Collatz.py
+++ Collatz.py
@@ -3,13 +3,14 @@
 # the Collatz conjecture, or n = 3n+1
 # written by Colm Doherty on 2018-02-08
 
-# This function returns the Collatz sequence for 'n' until it falls to 1
+# This function returns the Collatz sequence for 'n' until it falls to 1.
 # The Collatz Conjecture states that, starting with a positive Integer,
 # if the previous term is even, and you divide it by 2, or if it is
 # odd, and you multiply it by 3 and add 1, then no matter what term 
-# you start with, the sequence will always reach 1. 
+# you start with, the sequence will always reach 1. We are given a 
+# start value for 'n'of 17.
 
-n = 17      #state the chosen start value
+n = 17      #state the given start value
 print (n)   #display the chosen start value
 
 while n <=10000 and (n != 1) :
